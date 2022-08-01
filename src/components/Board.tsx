8b9d6e15335e14af973a03c376b4d9071ba33d6f
jadefred <97032726+jadefred@users.noreply.github.com>
2022-08-01 16:39:13 +0200

    added basic board outline in css

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,7 +1,10 @@
+import * as React from "react";
 import { FC } from "react";
 
-export interface Props {}
+export interface Props {
+  children: React.ReactNode;
+}
 
 export const Board: FC<Props> = (props) => {
-  return <div className="board"></div>;
+  return <div className="board">{props.children}</div>;
 };
--- src/components/Square.tsx
+++ src/components/Square.tsx
@@ -1,3 +1,4 @@
+import * as React from "react";
 import { FC } from "react";
 
 export interface Props {}
