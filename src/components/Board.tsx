6ee1df8dc3d41610ababaa62a6189c9b7d25a780
jadefred <97032726+jadefred@users.noreply.github.com>
2022-08-01 16:24:24 +0200

    Added Board and Square components to App

---
+++ src/components/Board.tsx
@@ -0,0 +1,7 @@
+import { FC } from "react";
+
+export interface Props {}
+
+export const Board: FC<Props> = (props) => {
+  return <div className="board"></div>;
+};
---
+++ src/components/Square.tsx
@@ -0,0 +1,7 @@
+import { FC } from "react";
+
+export interface Props {}
+
+export const Square: FC<Props> = (props) => {
+  return <div className="square"></div>;
+};
