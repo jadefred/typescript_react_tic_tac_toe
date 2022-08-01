9fa5b899f9e6fa999fe687a6c72698fecf8ffc6c
jadefred <97032726+jadefred@users.noreply.github.com>
2022-08-01 18:06:38 +0200

    Click square to display X accordingly

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import { Board } from "./components/Board";
 import { Square } from "./components/Square";
 
+const defaultSquare = (): (string | null)[] => new Array(9).fill(null);
+
 function App() {
+  const [square, setSquare] = useState(defaultSquare);
+
+  function handleSquareClick(index: number) {
+    let newSquare = square;
+    newSquare[index] = "x";
+    setSquare([...newSquare]);
+  }
+
   return (
     <main>
       <Board>
-        <Square />
-        <Square />
-        <Square />
-        <Square />
-        <Square />
-        <Square />
-        <Square />
-        <Square />
-        <Square />
+        {square.map((square, index: number) => (
+          <Square key={index} x={square === "x" ? 1 : 0} o={square === "o" ? 1 : 0} onClick={() => handleSquareClick(index)} />
+        ))}
       </Board>
     </main>
   );
--- src/components/Square.tsx
+++ src/components/Square.tsx
@@ -1,8 +1,16 @@
 import * as React from "react";
 import { FC } from "react";
 
-export interface Props {}
+export interface Props {
+  onClick?: () => void;
+  x: number;
+  o: number;
+}
 
 export const Square: FC<Props> = (props) => {
-  return <div className="square"></div>;
+  return (
+    <div className="square" {...props}>
+      {props.x ? "x" : props.o ? "o" : ""}
+    </div>
+  );
 };
